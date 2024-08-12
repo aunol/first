76f24bf83992987264cc320b056e192fe1ca4079
Jeongho Choi <hj2cjh@gmail.com>
2024-08-12 18:11:13 +0900

    Save current changes before switching commits

--- src/layouts/createUrl.js
+++ src/layouts/createUrl.js
@@ -1,4 +1,4 @@
 export const createUrl = (path) => {
-    const baseUrl = 'http://223.130.154.211:8080/'; // 기본 URL
+    const baseUrl = 'http://localhost:8080/'; // 기본 URL
     return `${baseUrl}${path.replace(/^\//, '')}`; // baseUrl과 path를 결합하여 전체 URL 생성
   };
